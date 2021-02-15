c44acdb2be568b008c7123a8a8894ce670ff0e08
João Freitas <joaoofreitas00@gmail.com>
2021-02-15 20:25:40 +0000

    🧼 Code cleaned and default arguments added (#2 #3)

--- main.py
+++ main.py
@@ -2,72 +2,74 @@
 
 from getopt import getopt
 from sys import argv
-from queue import Empty, Queue
 import socket
 import threading
 
-# usage: python3 main.py --user=User_2 --ip=127.0.0.1 --port=34000
-opts, argv = getopt(argv[1:], 'u:i:p:', ["username=", "ip=", "port="])
+username = 'Anonymous'
+PORT = 8080
+
+# usage: python3 main.py --user=User --port=8080
+opts, argv = getopt(argv[1:], 'u:i:p:', ["username=", "port="])
 for k, v in opts:
     if k == '-u' or k == '--username':
-        clientUsername = str(v)
-        print(clientUsername)
-    elif k == '-i' or k == '--ip':
-        localIP = str(v)
-        print(localIP)
+        username = str(v)
+        print(username)
     elif k == '-p' or k == '--port':
-        localPort = int(v)
-        print(localPort)
+        PORT = int(v)
+        print(PORT)
 
-
-addr = [] # If this is empty it means this client want's to start a message
+addr = []  # If this is empty it means this client want's to start a message
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-UDPServerSocket.bind((localIP, localPort)) # Better to get the IP automatically
+UDPServerSocket.bind(('0.0.0.0', PORT))  # Better to get the IP automatically
 
-bufferSize  = 1024
-def recieve():
+bufferSize = 1024
+
+
+def receive():
     global addr
     # Listen for incoming datagrams
-    while(True):
-        bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+    while True:
+        bytes_address_pair = UDPServerSocket.recvfrom(bufferSize)
 
-        message = bytesAddressPair[0] 
-        address = bytesAddressPair[1]
+        message = bytes_address_pair[0]
+        address = bytes_address_pair[1]
+        addr.append(address[0])
+        addr.append(address[1])
 
-        addr = address
-
-        packet = [hex(b) for b in message] # For debug purposes
+        print(addr)
+        packet = [hex(b) for b in message]  # For debug purposes
         message = message.decode('utf-8')
-        
+
         print(packet)
         print(message)
 
         if message.count('|') != 0:
-            spliter = message.split('|', 1)
-            username = spliter[0]
-            message = spliter[1]
+            splitter = message.split('|', 1)
+            client_username = splitter[0]
+            message = splitter[1]
 
-            print(f'[{username}] => {message}')
+            print(f'[{client_username}] => {message}')
+
 
 def send():
     global addr
     # Listen for incoming datagrams
-    while(True):
-        message = input("Your message: ") # Waits for user input
-        if addr == []: 
-            a = input("What's the target you want to send the message?: ")
-            p = input("In which port?: ")
-            addr[0] = a
-            addr[1] = p
-        else:
-            msg = f'{clientUsername}|{message}\n'
+    while True:
+        message = input('> ')  # Waits for user input
+        if not addr:
+            client_address = input("What's the target you want to send the message?: ")
+            client_port = input("In which port?: ")
+            addr.append(client_address)
+            addr.append(int(client_port))
+        elif message != '' or message != ' ':  # Need to put this null or empty right
+            msg = f'{username}|{message}\n'
             UDPServerSocket.sendto(msg.encode('utf-8'), (addr[0], addr[1]))
 
-inbound = threading.Thread(target=recieve)
+
+inbound = threading.Thread(target=receive)
 outbound = threading.Thread(target=send)
 inbound.start()
 outbound.start()
 
 inbound.join()
 outbound.join()
-    
