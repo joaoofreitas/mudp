fdb4d9c048cc869ff7e38d0825019d59aaeb64cc
João Freitas <joaoofreitas00@gmail.com>
2021-02-20 01:13:51 +0000

    👾 RSA Encryption implemented... #6

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ import threading
 import logging
 import rsa
 
-(PUBLIC_KEY, PRIVATE_KEY) = rsa.newkeys(512, poolsize=1)  # To implement
-username = 'Anonymous'
-PORT = 8080
 bufferSize = 1024
-peerPublicKey = ''
+(PUBLIC_KEY, PRIVATE_KEY) = rsa.newkeys(bufferSize, poolsize=1)
+username = 'Anonymous'
+peerPublicKey = rsa.PublicKey(0, 0)
+PORT = 8080
 debugMode = False
 
 # usage: python3 main.py --username=Username --port=8080 --debug
@@ -26,6 +26,7 @@ for k, v in opts:
         logging.basicConfig(level=logging.DEBUG)
 
 logging.debug(f'Public key: {PUBLIC_KEY}')
+logging.debug(f'Private key: {PRIVATE_KEY}')
 logging.debug(f'Username: {username}')
 logging.debug(f'Port: {PORT}')
 logging.debug(f'Debug: {debugMode}')
@@ -47,21 +48,32 @@ def receive():
         addr.append(address[1])
 
         packet = [hex(b) for b in message]
-        message = decompress(message)
-        message = rsa.decrypt()
-        message = message.decode('utf-8')
+        handshake = False
+        logging.debug(f'{peerPublicKey.e}')
 
-        logging.debug(f'Address: {addr}')
-        logging.debug(f'Raw Packet: {str(packet)}')
-        logging.debug(f'Decoded Message: {message}')
+        try:
+            key = message.decode('utf-8').split('|', 2)
+        except UnicodeDecodeError:
+            handshake = True
+        if key[0] == 'RSA' and peerPublicKey.e == 0:
+            peerPublicKey = rsa.PublicKey(int(key[1]), int(key[2]))
+            UDPServerSocket.sendto(f'RSA|{PUBLIC_KEY.n}|{PUBLIC_KEY.e}'.encode('utf-8'), (addr[0], addr[1]))
+            logging.debug(f'RSA Key Detected... Peer Public Key: {peerPublicKey}')
+            logging.debug(f'RAW Peer Public Key detected: {key}')
+        elif key[0] != 'RSA' or handshake:
+            message = decompress(message)
+            message = rsa.decrypt(message, PRIVATE_KEY)
+            message = message.decode('utf-8')
 
-        if message.count('|') != 0 and message:
-            splitter = message.split('|', 2)
-            client_username = splitter[0]
-            message = splitter[1]
-            peerPublicKey = splitter[2]
+            logging.debug(f'Address: {addr}')
+            logging.debug(f'Raw Packet: {str(packet)}')
+            logging.debug(f'Decoded Message: {message}')
 
-            print(f'[{client_username}] => {message}', end='')
+            if message.count('|') != 0 and message:
+                splitter = message.split('|', 1)
+                client_username = splitter[0]
+                message = splitter[1]
+                print(f'[{client_username}] => {message}', end='')
 
 
 def send():
@@ -69,11 +81,15 @@ def send():
     global peerPublicKey
     while True:
         message = str(input('>>> '))  # Waits for user input
-        if not addr:
+        if not addr or peerPublicKey.e == 0:
             client_address = input("What's the target you want to send the message?: ")
             client_port = input("In which port?: ")
             addr.append(client_address)
             addr.append(int(client_port))
+
+            # Exchange Public Keys using handshake
+            logging.debug(f'Sending handshake to {addr[0]}: PUBLIC KEY: {PUBLIC_KEY}')
+            UDPServerSocket.sendto(f'RSA|{PUBLIC_KEY.n}|{PUBLIC_KEY.e}'.encode('utf-8'), (addr[0], addr[1]))
         elif message:
             msg = f'{username}|{message}\n'
             msg = msg.encode('utf-8')
