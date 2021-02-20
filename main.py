ba802f3f8cec88adda01f6df8012159be311c9b7
João Freitas <joaoofreitas00@gmail.com>
2021-02-20 19:47:51 +0000

    ✍️ Refactor and code description

--- main.py
+++ main.py
@@ -8,14 +8,24 @@ import threading
 import logging
 import rsa
 
-bufferSize = 1024
-(PUBLIC_KEY, PRIVATE_KEY) = rsa.newkeys(bufferSize, poolsize=1)
-username = 'Anonymous'
-peerPublicKey = rsa.PublicKey(0, 0)
-PORT = 8080
-debugMode = False
 
-# usage: python3 main.py --username=Username --port=8080 --debug
+# Default variables
+username = 'Anonymous'
+PORT = 8080
+bufferSize = 1024
+
+# Generation and initialization of RSA Keys
+(PUBLIC_KEY, PRIVATE_KEY) = rsa.newkeys(bufferSize, poolsize=2)
+peerPublicKey = rsa.PublicKey(0, 0)
+
+# Initialization of socket and temporary address variable.
+addr = []  # If this is empty it means this client want's to start a message
+UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+UDPServerSocket.bind(('0.0.0.0', PORT))  # Better to get the IP automatically
+
+
+# Sets and creates the arguments and flags for initializing the program
+# Example usage: python3 main.py --username=Username --port=8080 --debug
 opts, argv = gnu_getopt(argv[1:], 'u:p:d:', ["username=", "port=", 'debug'])  # Fix me
 for k, v in opts:
     if k == '-u' or k == '--username':
@@ -25,63 +35,59 @@ for k, v in opts:
     elif k == '-d' or k == '--debug':
         logging.basicConfig(level=logging.DEBUG)
 
+# Debug Mode Logs
 logging.debug(f'Public key: {PUBLIC_KEY}')
 logging.debug(f'Private key: {PRIVATE_KEY}')
 logging.debug(f'Username: {username}')
 logging.debug(f'Port: {PORT}')
-logging.debug(f'Debug: {debugMode}')
-
-addr = []  # If this is empty it means this client want's to start a message
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-UDPServerSocket.bind(('0.0.0.0', PORT))  # Better to get the IP automatically
 
 
+# Receiving function thread. Takes care of handling RSA Public Keys Handshakes, getting and printing messages.
 def receive():
-    global addr
+    global addr  # Setting variables shared between threads
     global peerPublicKey
-    while True:
-        bytes_address_pair = UDPServerSocket.recvfrom(bufferSize)
+    while True:  # Looping to receive unless commands (to implement #5) or user control
+        datagram_packet = UDPServerSocket.recvfrom(bufferSize)  # Receives the packet with an expected buffer size
 
-        message = bytes_address_pair[0]
-        address = bytes_address_pair[1]
+        message = datagram_packet[0]
+        address = datagram_packet[1]
         addr.append(address[0])
         addr.append(address[1])
 
-        packet = [hex(b) for b in message]
-        handshake = False
-        logging.debug(f'{peerPublicKey.e}')
+        logging.debug(f'Address: {addr}')
+        logging.debug(f'Raw Packet: {str([hex(b) for b in message])}')
 
+        # Try to decode, if the message is compressed it will give a UnicodeDecodeError so it's an encrypted message
         try:
             key = message.decode('utf-8').split('|', 2)
-        except UnicodeDecodeError:
-            handshake = True
-        if key[0] == 'RSA' and peerPublicKey.e == 0:
-            peerPublicKey = rsa.PublicKey(int(key[1]), int(key[2]))
-            UDPServerSocket.sendto(f'RSA|{PUBLIC_KEY.n}|{PUBLIC_KEY.e}'.encode('utf-8'), (addr[0], addr[1]))
-            logging.debug(f'RSA Key Detected... Peer Public Key: {peerPublicKey}')
-            logging.debug(f'RAW Peer Public Key detected: {key}')
-        elif key[0] != 'RSA' or handshake:
+            # Does a second validation to check if it's a real public key handshake request
+            if key[0] == 'RSA' and peerPublicKey.e == 0:
+                peerPublicKey = rsa.PublicKey(int(key[1]), int(key[2]))
+                UDPServerSocket.sendto(f'RSA|{PUBLIC_KEY.n}|{PUBLIC_KEY.e}'.encode('utf-8'), (addr[0], addr[1]))
+
+                logging.debug(f'RSA Key Detected... Peer Public Key: {peerPublicKey}')
+                logging.debug(f'RAW Peer Public Key detected: {key}')
+        except UnicodeDecodeError:  # Handles has a normal compressed and encrypted message
             message = decompress(message)
             message = rsa.decrypt(message, PRIVATE_KEY)
             message = message.decode('utf-8')
 
-            logging.debug(f'Address: {addr}')
-            logging.debug(f'Raw Packet: {str(packet)}')
             logging.debug(f'Decoded Message: {message}')
 
-            if message.count('|') != 0 and message:
+            if message.count('|') != 0 and message:  # Separates username from message and print it...
                 splitter = message.split('|', 1)
                 client_username = splitter[0]
                 message = splitter[1]
                 print(f'[{client_username}] => {message}', end='')
 
 
+# Sending message thread. Handles user input and makes handshake requests and messages.
 def send():
     global addr
     global peerPublicKey
-    while True:
-        message = str(input('>>> '))  # Waits for user input
-        if not addr or peerPublicKey.e == 0:
+    while True:  # Loops to be always expecting user input
+        message = str(input('>>> '))  # User input sanitizing and prompting
+        if not addr or peerPublicKey.e == 0:  # Checks if a connection and handshake has been established
             client_address = input("What's the target you want to send the message?: ")
             client_port = input("In which port?: ")
             addr.append(client_address)
@@ -90,16 +96,17 @@ def send():
             # Exchange Public Keys using handshake
             logging.debug(f'Sending handshake to {addr[0]}: PUBLIC KEY: {PUBLIC_KEY}')
             UDPServerSocket.sendto(f'RSA|{PUBLIC_KEY.n}|{PUBLIC_KEY.e}'.encode('utf-8'), (addr[0], addr[1]))
-        elif message:
+        elif message:  # Creates the message in M-UDP format. Encodes, encrypts, compresses and sends it.
             msg = f'{username}|{message}\n'
             msg = msg.encode('utf-8')
             msg = rsa.encrypt(msg, peerPublicKey)
             msg = compress(msg)
 
-            logging.debug(f'Sending compressed message: {msg}')
             UDPServerSocket.sendto(msg, (addr[0], addr[1]))
+            logging.debug(f'Sending compressed message: {msg}')
 
 
+# Initializes and starts threads running a certain function (either send or receive)
 inbound = threading.Thread(name='Inbound', target=receive)
 outbound = threading.Thread(name="Outbound", target=send)
 inbound.start()
